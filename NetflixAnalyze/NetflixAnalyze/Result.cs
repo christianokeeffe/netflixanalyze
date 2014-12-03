0795f17930288771c6fa4f2db372eb4083d5cc49
Mette Pedersen <mettemuzen@hotmail.com>
2014-12-03 08:37:51 +0100

    nnm

---
+++ NetflixAnalyze/NetflixAnalyze/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetflixAnalyze
+{
+    class Result
+    {
+        double score;
+        int movieID;
+        int customerID;
+ 
+        public Result(double Score, int MovieID, int CustomerID)
+        {
+            score = Score;
+            movieID = MovieID;
+            customerID = CustomerID;
+        }
+    }
+}
